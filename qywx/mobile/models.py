50c15f7a71f7bbce999fe9abdbc0bc2003dbc889
Jianboo <chenjianbo_007@sina.com>
2015-07-22 14:59:11 +0800

    [REM] remove qywx director

--- qywx/mobile/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-# class mobile(models.Model):
-#     _name = 'mobile.mobile'
-
-#     name = fields.Char()
--- qywx/wx_manager/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import controllers
-import exception
-import models
-import wxmanager 
-import rpcutil 
